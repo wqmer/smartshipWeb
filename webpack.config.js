f085b4ad255ef73b6a26a1b831dd901a5d8f83f7
que2018 <goodiswill2016@gmail.com>
2021-12-11 14:44:53 -0800

    12112021

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,93 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' });
-}
+// if (process.env.NODE_ENV === "test") {
+//   require("dotenv").config({ path: ".env.test" });
+// } else if (process.env.NODE_ENV === "development") {
+//   require("dotenv").config({ path: ".env.development" });
+// }
 
 module.exports = (env) => {
-  const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css');
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
 
   return {
-    entry: ['babel-polyfill', './src/app.js'],
+    entry: ["babel-polyfill", "./src/app.js"],
     output: {
-      path: path.join(__dirname, 'public', 'dist'),
-      filename: 'bundle.js'
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js",
     },
     module: {
       rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }, 
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
 
-      {
-        test: /\.less$/,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader', options: {
-            sourceMap: true
-          }
-        }, {
-          loader: 'less-loader', options: {
-            sourceMap: true,
-            javascriptEnabled: true 
-          }
-        }]
-      },
-
-      {
-        test: /\.s?css$/,
-        use: CSSExtract.extract({
+        {
+          test: /\.less$/,
           use: [
             {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
+              loader: "style-loader",
             },
             {
-              loader: 'sass-loader',
+              loader: "css-loader",
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
+            {
+              loader: "less-loader",
+              options: {
+                sourceMap: true,
+                javascriptEnabled: true,
+              },
+            },
+          ],
+        },
 
-          ]   
-        })
-      },
-
-
-  ]
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
     },
-    plugins: [
-      CSSExtract,
-    ],
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
-      port:8081,
-      contentBase: path.join(__dirname, 'public'),
+      port: 8081,
+      contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
-      publicPath: '/dist/',
+      publicPath: "/dist/",
       proxy: {
-        '/api':    {
-                     target: 'https://smartship-web-api.herokuapp.com/',
-                     //target:  'http://[::1]:8000',
-                     pathRewrite: {'^/api' : ''},
-                     changeOrigin: true,
-                     secure: false
-        }    
-      }
-    }
+        "/api": {
+          target: "http://smartshipwebapi-env.eba-mevsg3bu.us-west-2.elasticbeanstalk.com/",
+          // target: "https://smartship-web-api.herokuapp.com/",
+          //target:  'http://[::1]:8000',
+          pathRewrite: { "^/api": "" },
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
   };
 };

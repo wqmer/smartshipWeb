892b3aecf5fb19a9c92c7ab184d9726787032a4e
que2018 <goodiswill2016@gmail.com>
2021-12-11 14:55:19 -0800

    12112021

--- src/container/shipping_user/ticket_page/index.js
+++ src/container/shipping_user/ticket_page/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom"
 import { connect } from "react-redux"
 import NotFound from "../../../components/notFound"
 import TicketAdd from "../../../components/Ticket/TicketAdd"
